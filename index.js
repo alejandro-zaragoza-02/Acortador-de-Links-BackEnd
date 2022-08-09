7b052a5baf65017bfa0227a02885b0fda934cdca
alejandro-zaragoza-02 <102815776+alejandro-zaragoza-02@users.noreply.github.com>
2022-08-09 13:04:11 +0200

    Sexto commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import redicRouter from './routes/redirect.route.js'
 import cookieParser from 'cookie-parser';
 
 const app = express();
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cookieParser());
